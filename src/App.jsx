fca51062f9d3f22a9168a49018b8c73fe01cd9cd
BearK <harry.hsu0515@gmail.com>
2022-12-01 11:37:16 +0800

    串接API 1.更新CRUD，與伺服器同步 2.用auth Token做身分驗證 3.用context共用身分狀態

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
+import { AuthProvider } from 'contexts/AuthContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.scss';
 import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
-
 
 function App() {
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
-          <Route path="login" element={<LoginPage />} />
-          <Route path="signup" element={<SignUpPage />} />
-          <Route path="todo" element={<TodoPage />} />
-          <Route path="*" element={<HomePage />} />
-        </Routes>
+        <AuthProvider>
+          <Routes>
+            <Route path="login" element={<LoginPage />} />
+            <Route path="signup" element={<SignUpPage />} />
+            <Route path="todos" element={<TodoPage />} />
+            <Route path="*" element={<HomePage />} />
+          </Routes>
+        </AuthProvider>
       </BrowserRouter>
     </div>
   );
